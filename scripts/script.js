3e51f76eef318359f08616afa278e98c2e86032b
MadeliefZandt <150804283+MadeliefZandt@users.noreply.github.com>
2023-11-30 12:10:19 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,21 @@
 // JavaScript Document
 console.log("hi");
+
+var menubutton = document.querySelector("button");
+
+menubutton.onclick = openMenu; 
+
+function openMenu() { 
+var menu = document.querySelector("nav");
+menu.classList.add("toonMenu");
+  console.log("hsdsgdsadgsd");
+}
+
+var sluitbutton = document.querySelector("nav button");
+
+sluitbutton.onclick = closeMenu; 
+
+function closeMenu() { 
+var menu = document.querySelector("nav");
+menu.classList.remove("toonMenu");
+}
